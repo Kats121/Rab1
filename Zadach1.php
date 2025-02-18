18cf83f3ed590f9f1a39091e7e951b971bd27ca0
Artyom <Batonovmisa@gmail.com>
2025-02-18 22:22:04 +0300

    61

---
+++ Zadach1.php
@@ -0,0 +1,14 @@
+<?php
+$i = readline ("Введите число для вычисления факториала");
+if ($i<=0) {
+echo "Ошибка! Введите положительное числою. \n";
+}
+else {
+$factor=1;
+$c = 1;
+while ($c<=$counter);
+$factorial *= $counter;
+        $counter++; 
+echo "Факториал числа $number равен $factorial.\n"
+}
+
